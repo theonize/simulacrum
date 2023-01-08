f1adcfd13d683b92fac5b7d4f194e10d97fe2d5c
fingerskier <fingerskier@gmail.com>
2023-01-08 17:02:56 -0600

    acts study skeleton

--- script/index.js
+++ script/index.js
@@ -1,7 +1,10 @@
 window.addEventListener('load', event=>{
-  if ("ontouchstart" in document.documentElement) { 
-    document.querySelector(".hint").innerHTML = "<p>Swipe left or right to navigate</p>";
-  }
-  
-  impress().init()
+  const container = document.getElementById('content')
+  const links = document.querySelectorAll('a')
+
+  // for (const link of links) {
+    a.onclick = event=>{
+      container.src = a.href
+    }
+  // }
 })
